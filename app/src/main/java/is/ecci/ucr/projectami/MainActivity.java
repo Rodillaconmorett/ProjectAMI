c443c0b0c8dfe777a37f6d4bf4680e9aed8208ab
Rodillaconmorett <danielmunozc95@gmail.com>
2017-05-09 04:57:47 -1200

    el commit del comentario

--- app/src/main/java/is/ecci/ucr/projectami/MainActivity.java
+++ app/src/main/java/is/ecci/ucr/projectami/MainActivity.java
@@ -10,4 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    // este es un ejemplo de un commit
 }
