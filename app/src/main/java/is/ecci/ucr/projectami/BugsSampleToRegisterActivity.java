ea440cebad41770cafc5e85180c0669f3ea626dc
Oscar Azofeifa <oscar.azofeifa.segura@gmail.com>
2017-05-22 00:18:14 -0600

    BugsSampleToRegisterActivity: Interfaz de usuario para añadir muestras de macroinvertebrados.

---
+++ app/src/main/java/is/ecci/ucr/projectami/BugsSampleToRegisterActivity.java
@@ -0,0 +1,13 @@
+package is.ecci.ucr.projectami;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BugsSampleToRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bugs_sample_to_register);
+    }
+}
