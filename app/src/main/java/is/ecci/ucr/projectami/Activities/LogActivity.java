c7b45a7e0cc7cf6d4c7377d1debc411eb2d40e5c
bgd95 <bjgd95@gmail.com>
2017-06-13 09:54:09 -0600

    log empezado

---
+++ app/src/main/java/is/ecci/ucr/projectami/Activities/LogActivity.java
@@ -0,0 +1,12 @@
+package is.ecci.ucr.projectami.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by bjgd9 on 12/6/2017.
+ */
+
+public class LogActivity extends AppCompatActivity {
+
+
+}
