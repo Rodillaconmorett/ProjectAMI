0c62b1fa52e639bb0e9f1aadc006852d181190e0
Oscar Azofeifa <oscar.azofeifa.segura@gmail.com>
2017-06-21 11:00:48 -0600

    Activities ViewPager para el inicio, tokenPrivilegios avance

--- app/src/main/java/is/ecci/ucr/projectami/TokenPrivilegios.java
+++ app/src/main/java/is/ecci/ucr/projectami/TokenPrivilegios.java
@@ -1,6 +1,10 @@
 package is.ecci.ucr.projectami;
 
+import android.widget.Switch;
+
 import java.util.BitSet;
+
+import is.ecci.ucr.projectami.Users.User;
 
 /**
  * Created by Oscar Azofeifa on 13/05/2017.
@@ -11,20 +15,65 @@ import java.util.BitSet;
 
 public class TokenPrivilegios {
 
-    /*Formato de los privilegios: PENDIENTE*/
-    private BitSet actionPrivileges;
-    private BitSet adminPrivileges;
-    int codKey;
-    String username;
-    String name;
+    /* PRIVILEGES FORMAT:
 
-    /* E:
-    *  S:
-    *  R:
+    *
+    * *******************************USERs TYPES
+    * Consultors Rights:
+    *   -See the map with sample points.
+    *   -Show general sample points data.
+    *   -Search sample points.
+    *   -Access to bugs catalog.
+    * Collectors Rights:
+    *   -Register samples sets.
+    *   -Make feedbacks based on images.
+    * Administrators Rights (Inherits Collector):
+    *   -Add, modify and delete sample points.
+    *   -Add, modify and delete users.
+    * BioAdministrators Rights:
+    *   -Add, modify and delete invertebrates.
+    *   -Make feedbacks without images.
     * */
-    public TokenPrivilegios(){
+
+    public enum UserProfile {
+        CONSULTOR,
+        COLLECTOR,
+        ADMINISTRATOR,
+        BIOADMINISTRATOR
+    }
+
+    private BitSet _actionPrivileges;
+    private BitSet _adminPrivileges;
+    private int _codKey;
+    private String _username;
+    private String _name;
+
+
+    /* E: User to create
+    *  S: Creates a token with privileges related with the user specified.
+    *  R: If input is different than any kind of user, creates a Consultor Token by default.
+    * */
+    public TokenPrivilegios(String username, String name, UserProfile kindaUser){
+
+        _username = username;
+        _name = name;
+
+        switch (kindaUser) {
+            case CONSULTOR:
+                break;
+            case COLLECTOR:
+                break;
+            case ADMINISTRATOR:
+                break;
+            case BIOADMINISTRATOR:
+                break;
+        }
+    }
+
+    public TokenPrivilegios(String username, String name, long customizedPrivilges){
 
     }
+
     /* E:
        S:
        R:
